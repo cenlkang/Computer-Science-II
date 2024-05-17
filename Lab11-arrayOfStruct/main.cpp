480ecf0f7a8d09cdb7e38b67c22a008e39306c92
Cen Li <cen.li@mtsu.edu>
2024-05-17 00:56:00 -0500

    Add files via upload

---
+++ Lab11-arrayOfStruct/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// declare the struct type here
+
+// define the array size here
+
+int main() {
+
+  // Declare variables and the array of struct 
+
+
+  // Assign the values of each card in the array of cards
+
+
+  // Display the cards
+
+  
+  return 0;
+}
